cff6158b6f542cea10646c0d81686b3738a676ac
Space Nerd <florianpospiech77@gmail.com>
2024-08-30 16:27:04 +0200

    simple player movement
    also fix inverse player pos :P

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::fs::read_to_string;
 
 use anathema::prelude::*;
 use anathema::component::*;
+use anathema::widgets::components::events::KeyState;
 
 #[derive(State)]
-struct PlayerPosition {
+struct PlayerState {
     pos_x: Value<i64>,
     pos_y: Value<i64>,
 }
@@ -13,7 +14,28 @@ struct Player;
 
 impl Component for Player {
     type Message = ();
-    type State = PlayerPosition;
+    type State = PlayerState;
+
+    fn on_key(
+        &mut self,
+        key: KeyEvent,
+        state: &mut Self::State,
+        elements: Elements<'_, '_>,
+        context: Context<'_, Self::State>,
+    ) {
+        let mut pos_x = state.pos_x.to_mut();
+        let mut pos_y = state.pos_y.to_mut();
+        
+        if matches!(key.state, KeyState::Press) {
+            match key.get_char() {
+                Some('w') => *pos_y -= 1,
+                Some('a') => *pos_x -= 1,
+                Some('s') => *pos_y += 1,
+                Some('d') => *pos_x += 1,
+                _ => ()
+            }
+        }
+    }
 }
 
 fn main() {
@@ -22,16 +44,18 @@ fn main() {
     let doc = Document::new(main_template);
 
     let mut backend = TuiBackend::builder()
+        .enable_alt_screen()
+        .hide_cursor()
         .finish()
         .unwrap();
 
     let mut runtime = Runtime::builder(doc, backend);
 
-    let comp_id = runtime.register_component(
+    let _ = runtime.register_component(
         "player",
         "templates/player.aml",
         Player, 
-        PlayerPosition {
+        PlayerState {
             pos_x: 24.into(),
             pos_y: 24.into()
         }
