2da33320226b814318c287eb9ee56519603ef221
Space Nerd <florianpospiech77@gmail.com>
2024-08-30 15:52:25 +0200

    create a simple player

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,41 @@
 use std::fs::read_to_string;
 
 use anathema::prelude::*;
+use anathema::component::*;
+
+#[derive(State)]
+struct PlayerPosition {
+    pos_x: Value<i64>,
+    pos_y: Value<i64>,
+}
+
+struct Player;
+
+impl Component for Player {
+    type Message = ();
+    type State = PlayerPosition;
+}
 
 fn main() {
-    let template = read_to_string("template.aml").unwrap();
+    let main_template = read_to_string("templates/main.aml").unwrap();
 
-    let mut doc = Document::new(template);
+    let doc = Document::new(main_template);
 
     let mut backend = TuiBackend::builder()
-        .enable_raw_mode()
-        .hide_cursor()
         .finish()
         .unwrap();
 
     let mut runtime = Runtime::builder(doc, backend);
+
+    let comp_id = runtime.register_component(
+        "player",
+        "templates/player.aml",
+        Player, 
+        PlayerPosition {
+            pos_x: 24.into(),
+            pos_y: 24.into()
+        }
+    );
+
     runtime.finish().unwrap().run();
 }
