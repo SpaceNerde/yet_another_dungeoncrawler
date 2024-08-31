2fb7967d6ae75fb7fedc356c0de73cf0067163d6
Space Nerd <florianpospiech77@gmail.com>
2024-08-31 15:54:29 +0200

    complete reset

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,13 @@
-use std::fs::read_to_string;
+use rltk::{GameState, Rltk, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{max, min};
+use specs_derive::Component;
 
-use anathema::prelude::*;
-use anathema::component::*;
-use anathema::widgets::components::events::KeyState;
-
-#[derive(State)]
-struct PlayerState {
-    pos_x: Value<i64>,
-    pos_y: Value<i64>,
-}
-
-struct Player;
-
-impl Component for Player {
-    type Message = ();
-    type State = PlayerState;
-
-    fn on_key(
-        &mut self,
-        key: KeyEvent,
-        state: &mut Self::State,
-        elements: Elements<'_, '_>,
-        context: Context<'_, Self::State>,
-    ) {
-        let mut pos_x = state.pos_x.to_mut();
-        let mut pos_y = state.pos_y.to_mut();
-        
-        if matches!(key.state, KeyState::Press) {
-            match key.get_char() {
-                Some('w') => *pos_y -= 1,
-                Some('a') => *pos_x -= 1,
-                Some('s') => *pos_y += 1,
-                Some('d') => *pos_x += 1,
-                _ => ()
-            }
-        }
-    }
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
 }
 
 fn main() {
-    let main_template = read_to_string("templates/main.aml").unwrap();
-
-    let doc = Document::new(main_template);
-
-    let mut backend = TuiBackend::builder()
-        .enable_alt_screen()
-        .hide_cursor()
-        .finish()
-        .unwrap();
-
-    let mut runtime = Runtime::builder(doc, backend);
-
-    let _ = runtime.register_component(
-        "player",
-        "templates/player.aml",
-        Player, 
-        PlayerState {
-            pos_x: 24.into(),
-            pos_y: 24.into()
-        }
-    );
-
-    runtime.finish().unwrap().run();
 }
